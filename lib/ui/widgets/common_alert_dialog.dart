20d5279938197fe2c5e19b621d7d7ba4f0910d08
thiru.r <thiru.r@pacewisdom.com>
2024-12-30 19:39:01 +0530

    Updated alert dialog common

---
+++ lib/ui/widgets/common_alert_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:waioz/utility/app_colors.dart';
+import 'package:waioz/utility/font_utils.dart';
+
+class CommonAlertDialog extends StatelessWidget {
+  final String title;
+  final String content;
+  final String contentOk;
+  final String contentCancel;
+  final VoidCallback onTapOk;
+  final VoidCallback onTapCancel;
+
+  const CommonAlertDialog({
+    Key? key,
+    required this.title,
+    required this.content,
+    required this.contentOk,
+    required this.contentCancel,
+    required this.onTapOk,
+    required this.onTapCancel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title,style: FontUtils.gabaritoStyle(color: AppColors.textColor,fontSize: 16,fontWeight: FontWeight.bold),),
+      content: Text(content,style: FontUtils.circularStdStyle(color: AppColors.textColor,fontSize: 14,fontWeight: FontWeight.w400),),
+      actions: [
+        TextButton(
+          onPressed: () {
+            onTapCancel;
+          },
+          child: Text(contentCancel,style: FontUtils.circularStdStyle(color: AppColors.textColor),),
+        ),
+        TextButton(
+          onPressed: () {
+            onTapOk;
+          },
+          child: Text(contentOk,style:FontUtils.circularStdStyle(color: AppColors.textColor)),
+        ),
+      ],
+    );
+  }
+
+}
