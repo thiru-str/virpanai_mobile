f6d7df399daf024bef1c5edefbf4b7f641879059
thiru.r <thiru.r@pacewisdom.com>
2025-05-03 13:53:28 +0530

    App name change

--- lib/utility/app_config.dart
+++ lib/utility/app_config.dart
@@ -1,13 +1,15 @@
 
 
 class AppConfig {
-  static const String appName = 'Cartel';
+  static const String appName = 'VirpanAi';
   static const String baseUrl = 'https://dev.cartel.waioz.com/';
+  // static const String baseUrl = 'https://cartel.waioz.com/';
   static const String googleApiKey = 'AIzaSyBYqO1N5Rr6fnLeOz4fxSPcPwHy77CNe_c';
   static const String publishableKeyStripe = 'pk_test_51QjKGFC5ZYai6Al85lKNzizk9i1E4ViPFg98dC4VECB9cEYPidOj6Pig2orwg1SKVGDndepP8McSJ4jPhnxhEIKj00aaPJefmb';
   static const String razorPayKey = 'rzp_test_TWZQg4tf6e1Tqs';
 
   // store test keys here
   /*static const String publishableKeyStripe = 'pk_test_51QjKGFC5ZYai6Al85lKNzizk9i1E4ViPFg98dC4VECB9cEYPidOj6Pig2orwg1SKVGDndepP8McSJ4jPhnxhEIKj00aaPJefmb';
-  static const String razorPayKey = 'rzp_test_TWZQg4tf6e1Tqs';*/
+  static const String razorPayKey = 'rzp_test_TWZQg4tf6e1Tqs';
+  */
 }
