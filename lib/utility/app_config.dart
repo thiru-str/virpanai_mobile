73e9c00d3dede1ee6cc97b2732159b3ab467d675
thiru.r <thiru.r@pacewisdom.com>
2025-05-12 20:57:15 +0530

    App and version name updated

--- lib/utility/app_config.dart
+++ lib/utility/app_config.dart
@@ -1,7 +1,7 @@
 
 
 class AppConfig {
-  static const String appName = 'Cartel';
+  static const String appName = 'Jayakrishna Flour Mills';
   static const String baseUrl = 'http://dashboard.jayakrishna.org:9000/';
   static const String googleApiKey = 'AIzaSyCDbSPUvo48SW3K2jrPBy4soGb8Y8YyhpI';
   static const String publishableKeyStripe = 'pk_test_51QjKGFC5ZYai6Al85lKNzizk9i1E4ViPFg98dC4VECB9cEYPidOj6Pig2orwg1SKVGDndepP8McSJ4jPhnxhEIKj00aaPJefmb';
