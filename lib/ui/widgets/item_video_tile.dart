c9a357857060210e8b31003ffaf3e00fa7b9de8f
thiru.r <thiru.r@pacewisdom.com>
2025-05-26 23:36:24 +0530

    new components added

---
+++ lib/ui/widgets/item_video_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+class ItemVideoTile extends StatefulWidget {
+  final String videoUrl;
+  final String title;
+
+  const ItemVideoTile({
+    super.key,
+    required this.videoUrl,
+    required this.title,
+  });
+
+  @override
+  State<ItemVideoTile> createState() => _ItemVideoTileState();
+}
+
+class _ItemVideoTileState extends State<ItemVideoTile> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.setLooping(true);
+        _controller.setVolume(0);
+        _controller.play(); // 🔁 Always autoplay
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: SizedBox(
+            width: 150,
+            height: 150,
+            child: _controller.value.isInitialized
+                ? VideoPlayer(_controller)
+                : Container(color: Colors.black12),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(
+          widget.title,
+          style: const TextStyle(fontWeight: FontWeight.w600),
+        ),
+      ],
+    );
+  }
+}
+
