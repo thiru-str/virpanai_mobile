88a87161e3e5ef61f34dfd8a391a880a7b5acae6
thiru.r <thiru.r@pacewisdom.com>
2025-06-25 11:15:44 +0530

    app update

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -186,7 +186,7 @@ class _HomePageState extends State<HomePage> {
         return Item8(content: homePageContent);
       case "item9":
         return Item9(content: homePageContent);
-      case "item10":
+      case "item10": // video
         return Item10(content: homePageContent);
       case "item11":
         return Item11(content: homePageContent);
