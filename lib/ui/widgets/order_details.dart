538e78df157d3aa1e8d3ec4615834269ccf698b7
thiru.r <thiru.r@pacewisdom.com>
2025-07-10 22:27:49 +0530

    Agent APIs update

--- lib/ui/widgets/order_details.dart
+++ lib/ui/widgets/order_details.dart
@@ -8,6 +8,7 @@ import 'package:waioz/ui/widgets/products_card.dart';
 import 'package:waioz/ui/widgets/store_location_widget.dart';
 import 'package:waioz/ui/widgets/store_summary_card.dart';
 import 'package:waioz/utility/app_assets.dart';
+import 'package:waioz/utility/app_utils.dart';
 
 import '../../api/api_service.dart';
 import '../../model/live_order_detail_response.dart';
@@ -17,14 +18,19 @@ import '../../utility/font_utils.dart';
 class OrderDetailsPage extends StatefulWidget {
   final String orderId;
   final bool isFromLiveOrder;
-  const OrderDetailsPage({Key? key,required this.orderId,this.isFromLiveOrder = false}) : super(key: key);
+
+  const OrderDetailsPage(
+      {Key? key, required this.orderId, this.isFromLiveOrder = false})
+      : super(key: key);
 
   @override
   State<OrderDetailsPage> createState() => _OrderDetailsPageState();
 }
+
 class _OrderDetailsPageState extends State<OrderDetailsPage> {
   LiveOrderDetailResponse? _liveOrderDetailResponse;
   bool apiLoading = true;
+  bool completeOrderLoading = false;
 
   @override
   void initState() {
@@ -34,118 +40,151 @@ class _OrderDetailsPageState extends State<OrderDetailsPage> {
   }
 
   Future<void> initApis() async {
-  getApis();
-}
+    getApis();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return apiLoading? Center(child: CircularProgressIndicator(color: AppColors.primary,),):Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CommonAppBar(title: widget.isFromLiveOrder?'Order Details':'Past Order',showBack: true,),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              StoreSummaryCard(
-                storeName: _liveOrderDetailResponse?.data?.shopName??'',
-                address: _liveOrderDetailResponse?.data?.shopAddress??'',
-                totalPrice: _liveOrderDetailResponse?.data?.totalPrice??'',
-              ),
-              // StoreLocationCard(
-              //   icon: const Icon(Icons.location_on, color: Colors.teal, size: 32),
-              //   mapWidget: Image.asset(
-              //     AppAssets.ic_map,
-              //     fit: BoxFit.cover,
-              //   ),
-              // ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0,vertical: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children:[ const Text(
-                    'List of Products',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
+    return apiLoading
+        ? Center(
+            child: CircularProgressIndicator(
+              color: AppColors.primary,
+            ),
+          )
+        : Scaffold(
+            backgroundColor: Colors.white,
+            appBar: CommonAppBar(
+              title: widget.isFromLiveOrder ? 'Order Details' : 'Past Order',
+              showBack: true,
+            ),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StoreSummaryCard(
+                      storeName: _liveOrderDetailResponse?.data?.shopName ?? '',
+                      address:
+                          _liveOrderDetailResponse?.data?.shopAddress ?? '',
+                      totalPrice:
+                          _liveOrderDetailResponse?.data?.totalPrice ?? '',
                     ),
-                  ),
+                    // StoreLocationCard(
+                    //   icon: const Icon(Icons.location_on, color: Colors.teal, size: 32),
+                    //   mapWidget: Image.asset(
+                    //     AppAssets.ic_map,
+                    //     fit: BoxFit.cover,
+                    //   ),
+                    // ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 16),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'List of Products',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Visibility(
+                              visible: !widget.isFromLiveOrder,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 4),
+                                decoration: BoxDecoration(
+                                  color: Colors.green.withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(color: Colors.green),
+                                ),
+                                child: const Text(
+                                  "Yet to progress",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.green,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ]),
+                    ),
+                    ListView.builder(
+                      itemCount:
+                          _liveOrderDetailResponse?.data?.products?.length ?? 0,
+                      physics: const NeverScrollableScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        final item =
+                            _liveOrderDetailResponse?.data?.products?[index];
+                        return ProductsCard(
+                          imageUrl: item?.productImage ?? '',
+                          title: item?.productTitle ?? '',
+                          productCount: item?.quantity ?? '',
+                          price: item?.total ?? '',
+                        );
+                      },
+                    ),
                     Visibility(
-                      visible: !widget.isFromLiveOrder,
-                      child: Container(
+                      visible:
+                          (_liveOrderDetailResponse?.data?.fulfillmentId ?? '')
+                              .isEmpty,
+                      child: Padding(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 12, vertical: 4),
-                        decoration: BoxDecoration(
-                          color:  Colors.green.withOpacity(0.1),
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(color: Colors.green),
-                        ),
-                        child: const Text(
-                          "Yet to progress",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.green,
+                            horizontal: 16.0, vertical: 8),
+                        child: completeOrderLoading? Center(child: CircularProgressIndicator(color: AppColors.primary,),):ElevatedButton(
+                          onPressed: () async {
+                            setState(() {
+                              completeOrderLoading = true;
+                            });
+                            final response = await ApiService().completeOrder(
+                                context,
+                                _liveOrderDetailResponse?.data?.orderId ?? '',
+                                _liveOrderDetailResponse?.data?.fulfillmentId ??
+                                    '');
+                            setState(() {
+                              completeOrderLoading = false;
+                            });
+                            if (response != null) {
+                              if (response['message'] != null) {
+                                AppUtils.showToast(response['message']);
+                              }
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primary, // Button color
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            minimumSize: const Size(double.infinity, 60),
+                          ),
+                          child: Text(
+                            'Mark as Completed',
+                            style: FontUtils.primaryFontStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ]
+                  ],
                 ),
               ),
-          ListView.builder(
-            itemCount: _liveOrderDetailResponse?.data?.products?.length??0,
-            physics: const NeverScrollableScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              final item = _liveOrderDetailResponse?.data?.products?[index];
-              return ProductsCard(
-                    imageUrl: item?.productImage??'',
-                    title: item?.productTitle??'',
-                    productCount: item?.quantity??'',
-                    price: item?.total??'',
-                  );
-            },
-          ),
-              Visibility(
-                visible: (_liveOrderDetailResponse?.data?.fulfillmentId??'').isEmpty,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 8),
-                  child: ElevatedButton(
-                    onPressed: () {
-
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.primary, // Button color
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      minimumSize: const Size(double.infinity, 60),
-                    ),
-                    child: Text(
-                      'Mark as Completed',
-                      style: FontUtils.primaryFontStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 
   void getApis() async {
     try {
       final ApiService apiService = ApiService();
-      final orderDetailResponse = await apiService.orderDetail(context,widget.orderId);
+      final orderDetailResponse =
+          await apiService.orderDetail(context, widget.orderId);
       setState(() {
         _liveOrderDetailResponse = orderDetailResponse;
         apiLoading = false;
