cc13a24c6267be22a07729bd20b2353b3c0fa020
thiru.r <thiru.r@pacewisdom.com>
2025-07-18 23:13:52 +0530

    product count variation

--- lib/utility/app_config.dart
+++ lib/utility/app_config.dart
@@ -2,8 +2,8 @@
 
 class AppConfig {
   static const String appName = 'VirpanAi';
-  static const String baseUrl = 'https://virpanai.dev.api.waioz.com/';
-  //static const String baseUrl = 'https://virpanai.api.waioz.com/';
+  // static const String baseUrl = 'https://virpanai.dev.api.waioz.com/';
+  static const String baseUrl = 'https://virpanai.api.waioz.com/';
   static const String googleApiKey = 'AIzaSyBYqO1N5Rr6fnLeOz4fxSPcPwHy77CNe_c';
   static const String publishableKeyStripe = 'pk_test_51QjKGFC5ZYai6Al85lKNzizk9i1E4ViPFg98dC4VECB9cEYPidOj6Pig2orwg1SKVGDndepP8McSJ4jPhnxhEIKj00aaPJefmb';
   static const String razorPayKey = 'rzp_test_YRkMaoYHJhpYjV';
