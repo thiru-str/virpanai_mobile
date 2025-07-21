15336678f37242b12571e223ffd28eb87241d62a
thiru.r <thiru.r@pacewisdom.com>
2025-07-21 20:12:25 +0530

    Image Update

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -18,6 +18,7 @@ import 'package:waioz/ui/widgets/home/grid_2.dart';
 import 'package:waioz/ui/widgets/home/item_9.dart';
 import 'package:waioz/ui/widgets/home/slider_1.dart';
 import 'package:waioz/ui/widgets/home/grid_1.dart';
+import 'package:waioz/ui/widgets/item_8.dart';
 import 'package:waioz/ui/widgets/search_address.dart';
 import 'package:waioz/ui/widgets/home/item_1.dart';
 import 'package:waioz/ui/widgets/home/item_2.dart';
@@ -198,6 +199,8 @@ class _HomePageState extends State<HomePage> {
         return Item6(content: homePageContent);
       case "item7":
         return Item7(content: homePageContent);
+      case "item8":
+        return Item8(content: homePageContent);
       case "Slider3":
         return Slider3(content: homePageContent);
       case "Grid1":
