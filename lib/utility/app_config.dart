a1ef001c832338095898fc423ec1de877cfa16fa
thiru.r <thiru.r@pacewisdom.com>
2025-07-30 14:46:33 +0530

    package update

--- lib/utility/app_config.dart
+++ lib/utility/app_config.dart
@@ -2,7 +2,7 @@
 
 class AppConfig {
   static const String appName = 'Lakshaya Silks';
-  static const String baseUrl = 'https://dashboard.lakshayasilksarees.com/';
+  static const String baseUrl = 'https://api.lakshayasilksarees.com/';
   static const String googleApiKey = 'AIzaSyCfX_9jdhee806Q7Iupoz_3D71VzKGic5Y';
   static const String publishableKeyStripe = 'pk_test_51I56gHEwDIxehi3LSgIYEBR1HPdlR2qPW3ZT5BHYwbSWh0gc1mROdgj12GKlNScWZR3wrfB1Xbhqo6Wn1FCrv740002qSgpiRF';
   static const String razorPayKey = 'rzp_test_Feu8xMTJuXDx34';
