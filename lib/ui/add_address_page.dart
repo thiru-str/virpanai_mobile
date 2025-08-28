edfab3b3bf9bcea559e80c0216e1363a295fb5d5
thiru.r <thiru.r@pacewisdom.com>
2025-08-28 19:02:17 +0530

    Address fixes

--- lib/ui/add_address_page.dart
+++ lib/ui/add_address_page.dart
@@ -62,6 +62,9 @@ class _AddAddressPage extends State<AddAddressPage> {
       firstNameController.text = address.firstName ?? '';
       lastNameController.text = address.lastName ?? '';
       phoneNumberController.text = address.phone ?? '';
+      cityController.text = address.city??'';
+      stateController.text = address.province??'';
+      zipCodeController.text = address.postalCode ?? '';
       selectedLocation = address.addressName ?? AppStrings.home;
 
       if (selectedLocation != AppStrings.home && selectedLocation != AppStrings.work) {
