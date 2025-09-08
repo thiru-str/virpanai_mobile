ed0021bcef7131d03f63badbe8472ad1771b1e2b
thiru.r <thiru.r@pacewisdom.com>
2025-09-08 17:02:36 +0530

    multiple button fix

--- lib/ui/add_address_page.dart
+++ lib/ui/add_address_page.dart
@@ -49,6 +49,7 @@ class _AddAddressPage extends State<AddAddressPage> {
 
   String selectedLocation = AppStrings.home; // Default location selection
   bool apiCalling = true;
+  bool isUpdating = false;
   RegisterResponse? registerResponse;
   double latitude = 0;
   double longitude = 0;
@@ -267,7 +268,7 @@ class _AddAddressPage extends State<AddAddressPage> {
               SafeArea(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
+                  child: isUpdating? Center(child: CircularProgressIndicator(color: AppColors.primary,),):ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
                         print("Form is valid. Proceed to Create Address.");
@@ -372,6 +373,9 @@ class _AddAddressPage extends State<AddAddressPage> {
 
   void createOrUpdateAddress() async {
     try {
+      setState(() {
+        isUpdating = true;
+      });
       final ApiService apiService = ApiService();
       selectedLocation = selectedLocation == AppStrings.others
           ? otherAddressName.text
@@ -391,7 +395,7 @@ class _AddAddressPage extends State<AddAddressPage> {
           latitude.toString(),
           longitude.toString());
       setState(() {
-        apiCalling = false;
+        isUpdating = false;
       });
       Navigator.pop(context, true);
       if (widget.doublePop) {
@@ -399,7 +403,7 @@ class _AddAddressPage extends State<AddAddressPage> {
       }
     } catch (e) {
       setState(() {
-        apiCalling = false;
+        isUpdating = false;
       });
       print(e);
     }
