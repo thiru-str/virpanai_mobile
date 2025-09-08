41208e0a3097ffcb8a491d483248c7d61e745524
thiru.r <thiru.r@pacewisdom.com>
2025-09-08 21:26:20 +0530

    street address fix

--- lib/ui/add_address_page.dart
+++ lib/ui/add_address_page.dart
@@ -64,6 +64,7 @@ class _AddAddressPage extends State<AddAddressPage> {
       firstNameController.text = address.firstName ?? '';
       lastNameController.text = address.lastName ?? '';
       phoneNumberController.text = address.phone ?? '';
+      streetAddressController.text = '${address.address1 ?? ''} ${address.address2 ?? ''}';
       cityController.text = address.city??'';
       stateController.text = address.province??'';
       zipCodeController.text = address.postalCode ?? '';
