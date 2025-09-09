fbd31f88fb3d5a096685897ec2fe587487235456
thiru.r <thiru.r@pacewisdom.com>
2025-09-09 18:34:21 +0530

    app url changes

--- lib/utility/app_config.dart
+++ lib/utility/app_config.dart
@@ -2,7 +2,7 @@
 
 class AppConfig {
   static const String appName = 'Build Merchant';
-  static const String baseUrl = 'https://dashboard.buildmerchant.in/';
+  static const String baseUrl = 'https://api.buildmerchant.in/';
   static const String googleApiKey = 'AIzaSyBYqO1N5Rr6fnLeOz4fxSPcPwHy77CNe_c';
   static const String publishableKeyStripe = 'pk_test_51QjKGFC5ZYai6Al85lKNzizk9i1E4ViPFg98dC4VECB9cEYPidOj6Pig2orwg1SKVGDndepP8McSJ4jPhnxhEIKj00aaPJefmb';
   static const String razorPayKey = 'rzp_test_TWZQg4tf6e1Tqs';
