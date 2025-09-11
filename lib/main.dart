fe67a97c3d3d7902b66396b726c3fc25fc4f3262
thiru.r <thiru.r@pacewisdom.com>
2025-09-11 13:27:42 +0530

    Push Notification

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ import 'package:flutter/material.dart';
 
 import 'api/api_service.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -71,6 +73,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       home: SplashPage(skipLogin: skipLogin),
     );
