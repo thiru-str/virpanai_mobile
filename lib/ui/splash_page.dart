ecefe35f48f89224cec25253234f1396955d73c2
thiru.r <thiru.r@pacewisdom.com>
2025-09-14 21:52:42 +0530

    force update

--- lib/ui/splash_page.dart
+++ lib/ui/splash_page.dart
@@ -1,25 +1,31 @@
 
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:waioz/ui/bottom_nav_page.dart';
+import 'package:waioz/ui/soft_update_bottom_sheet.dart';
 import 'package:waioz/ui/welcome_page.dart';
 import 'package:waioz/utility/app_assets.dart';
 import 'package:waioz/utility/app_colors.dart';
 import 'package:waioz/utility/page_route_utils.dart';
 
 import '../api/push_notification_service.dart';
+import '../model/public_detail_model.dart';
 import '../utility/shared_preferences_util.dart';
 
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'force_update_page.dart';
+
 class SplashPage extends StatefulWidget {
   final bool skipLogin;
-  const SplashPage({super.key,this.skipLogin = false});
+  final PublicDetailsResponse? publicDetailsResponse;
+  const SplashPage({super.key,this.skipLogin = false,this.publicDetailsResponse});
 
   @override
   State<SplashPage> createState() => _SplashPageState();
