d758dddb295b1c27d35b17edd5bbdf6fbb9be5b7
thiru.r <thiru.r@pacewisdom.com>
2025-09-19 08:22:25 +0530

    App icon update

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -387,7 +387,7 @@ class ApiService {
       queryParams['max_price'] = maxPrice;
     }
 
-    //queryParams['order'] = sortBy == AppStrings.low_high ? 'price' : '-price';
+    queryParams['order'] = sortBy == AppStrings.low_high ? 'price' : '-price';
 
     if (searchString.isNotEmpty) {
       queryParams['q'] = searchString;
@@ -397,7 +397,7 @@ class ApiService {
     queryParams['limit'] = limit.toString();
 
     return _makeGetRequest<ProductsResponse>(
-      'store/products',
+      'store/list-products',
       null,
       queryParams,
           (json) => ProductsResponse.fromJson(json),
