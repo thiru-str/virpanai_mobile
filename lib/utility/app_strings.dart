3783eec63e4a217741f6d8534e7f502a96471428
thiru.r <thiru.r@pacewisdom.com>
2025-09-24 17:39:08 +0530

    Push notificaton + tag redirection

--- lib/utility/app_strings.dart
+++ lib/utility/app_strings.dart
@@ -189,6 +189,7 @@ class AppStrings {
 
   static const String reDirectSearch = 'Search';
   static const String reDirectProduct = 'Product';
+  static const String reDirectOrder = 'Order';
   static const String reDirectLink = 'Link';
 
   static const String exitApp = 'Exit App?';
