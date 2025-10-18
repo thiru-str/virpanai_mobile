84076f917bda6057fb97ee670f96f5dcbd1bdc6c
thiru.r <thiru.r@pacewisdom.com>
2025-10-18 19:19:00 +0530

    Background image and color logic for components

--- lib/utility/app_strings.dart
+++ lib/utility/app_strings.dart
@@ -190,7 +190,6 @@ class AppStrings {
 
   static const String reDirectSearch = 'Search';
   static const String reDirectProduct = 'Product';
-  static const String reDirectPage = 'Page';
   static const String reDirectOrder = 'Order';
   static const String reDirectPage = 'Page';
   static const String reDirectLink = 'Link';
