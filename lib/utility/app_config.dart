5e2e6172ff18386dfa491c50e0d749c898d067c3
haripacewisdom <hariharaprabu.m@pacewisdom.com>
2026-02-17 21:41:38 +0530

    release update

--- lib/utility/app_config.dart
+++ lib/utility/app_config.dart
@@ -2,8 +2,8 @@
 
 class AppConfig {
   static const String appName = 'GoWelMart';
-  //static const String baseUrl = 'https://api.ecommerce.gowelmart.com/';
-  static const String baseUrl = 'https://api.host.getvirpanai.in/';
+  static const String baseUrl = 'https://api.ecommerce.gowelmart.com/';
+  //static const String baseUrl = 'https://api.host.getvirpanai.in/';
   static const String googleApiKey = 'AIzaSyBYqO1N5Rr6fnLeOz4fxSPcPwHy77CNe_c';
   static const String publishableKeyStripe = 'pk_test_51QjKGFC5ZYai6Al85lKNzizk9i1E4ViPFg98dC4VECB9cEYPidOj6Pig2orwg1SKVGDndepP8McSJ4jPhnxhEIKj00aaPJefmb';
   static const String razorPayKey = 'rzp_test_TWZQg4tf6e1Tqs';
