caa750eeb2b970090bbc190d95afa0d52989e84d
haripacewisdom <hariharaprabu.m@pacewisdom.com>
2026-02-19 15:57:29 +0530

    price changes

--- lib/ui/widgets/product_card_1.dart
+++ lib/ui/widgets/product_card_1.dart
@@ -33,12 +33,12 @@ class _ProductCard1State extends State<ProductCard1> {
 
     Variant? cheapest;
     for (final v in p.variants!) {
-      final calc = double.tryParse(v.calculatedPrice?.rawCalculatedAmount?.value ?? '');
+      final calc = double.tryParse(v.calculatedPrice?.calculatedAmount?.toString() ?? '');
       if (calc == null) continue;
       if (cheapest == null) {
         cheapest = v;
       } else {
-        final cheapestCalc = double.tryParse(cheapest.calculatedPrice?.rawCalculatedAmount?.value ?? '') ?? double.infinity;
+        final cheapestCalc = double.tryParse(cheapest.calculatedPrice?.calculatedAmount?.toString() ?? '') ?? double.infinity;
         if (calc < cheapestCalc) {
           cheapest = v;
         }
@@ -56,11 +56,11 @@ class _ProductCard1State extends State<ProductCard1> {
     final cheapest = _cheapestVariant(product);
 
     final calc = cheapest != null
-        ? double.tryParse(cheapest.calculatedPrice?.rawCalculatedAmount?.value ?? '')
+        ? double.tryParse(cheapest.calculatedPrice?.calculatedAmount?.toString() ?? '')
         : null;
 
     final orig = cheapest != null
-        ? double.tryParse(cheapest.calculatedPrice?.rawOriginalAmount?.value ?? '')
+        ? double.tryParse(cheapest.calculatedPrice?.originalAmount?.toString() ?? '')
         : null;
 
     final hasDiscount = (orig != null && calc != null && orig > calc);
