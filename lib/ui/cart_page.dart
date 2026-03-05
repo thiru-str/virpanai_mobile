674cc10dc594a1ed2ccc0f9c99ef1e1d5339ea40
Thiru <thiru@waioz.com>
2026-03-05 11:16:36 +0530

    fixes

--- lib/ui/cart_page.dart
+++ lib/ui/cart_page.dart
@@ -855,6 +855,7 @@ class _CartPageState extends State<CartPage>
             orderId: response.order?.id ?? '',
           ));
     } catch (e) {
+      getCartApi();
       setState(() {
         cartLoading = false;
       });
@@ -865,7 +866,7 @@ class _CartPageState extends State<CartPage>
   void makeRazorPayCall(String orderId) {
     var options = {
       'key': _getProviderKey(pp_id, paymentProviders),
-      'amount': cartResponse!.cart!.total!.toStringAsFixed(2),
+      'amount': ((cartResponse?.cart?.total??1) * 100).round(),
       'name': AppConfig.appName,
       'description': '${AppStrings.payment_to} ${AppConfig.appName}',
       'order_id': orderId,
